83690700ecfe5b9d5c12dfd97ebf83138d8d4a30
Victor Pereira <victorruan135@gmail.com>
2021-09-03 11:30:15 -0300

    Registration class is responsible to show registration the simple interface 👀. That's where the graphical part of the system is built ✔.

---
+++ src/br/ufma/ecp/victor/interfacegrafica/MainInterface.java
@@ -0,0 +1,9 @@
+package br.ufma.ecp.victor.interfacegrafica;
+
+
+public class MainInterface {
+    public static void main(String[] args) {
+        Registration cad = new Registration("Cadastro");
+        cad.setVisible(true);
+    }
+}
---
+++ src/br/ufma/ecp/victor/interfacegrafica/Registration.java
@@ -0,0 +1,54 @@
+package br.ufma.ecp.victor.interfacegrafica;
+
+import br.ufma.ecp.victor.user.User;
+import br.ufma.ecp.victor.dao.UserDAO;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Registration extends JFrame {
+
+    private JTextField txtNome;
+    private JTextField txtEmail;
+
+
+    public Registration(String Title) {
+        super(Title);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        setSize(300, 300);
+
+        JPanel panNome = new JPanel(); // Painel de nome
+        panNome.setLayout(new FlowLayout());
+        txtNome = new JTextField(15);
+        panNome.add(new JLabel("Nome"));
+        panNome.add(txtNome);
+
+        JPanel panEmail = new JPanel(); // Painel de Email
+        panEmail.setLayout(new FlowLayout());
+        txtEmail = new JTextField(15);
+        panEmail.add(new JLabel("Email"));
+        panEmail.add(txtEmail);
+
+        JButton btnSalvar = new JButton("Salvar"); // Botão salvar
+
+        btnSalvar.addActionListener( ( snuff ) -> {
+
+            User user = new User( txtNome.getText(), txtEmail.getText()); // cria um user
+
+            UserDAO dao = new UserDAO(); // cria um dao para o usuário fazer as operações de CRUD (manipular)
+
+            dao.createUser(user);
+
+        });
+
+        JButton btnFechar = new JButton("Fechar"); // Botão Fechar
+        btnFechar.addActionListener( ( e ) -> System.exit(0) ); // notação lambda: ( coisa ) -> Classe.metodo();
+
+        add(panNome);
+        add(panEmail);
+        add(btnSalvar);
+        add(btnFechar);
+    }
+}
